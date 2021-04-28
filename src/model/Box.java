9523c6b655d99edc4b93c13e04567cf94d162f19
OrtizBrayan <brayanortizbastidas@gmail.com>
2021-04-28 02:06:46 -0500

    Added box class content

--- src/model/Box.java
+++ src/model/Box.java
@@ -4,11 +4,49 @@ public class Box {
 	private int boxNumber;
 	private int row;
 	private int col;
+	private char snake;
+	private int ladder;
+	private String players;
 	
-	public Box(int r, int c) {
+	public Box(int r, int c, int b) {
 		row = r;
 		col = c;
+		boxNumber = b;
 	}
 	
+	public int getBoxNumber() {
+		return boxNumber;
+	}
 	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public char getSnake() {
+		return snake;
+	}
+	
+	public void setSnake(char s) {
+		snake = s;
+	}
+
+	public int getLadder() {
+		return ladder;
+	}
+
+	public void setLadder(int ladder) {
+		this.ladder = ladder;
+	}
+
+	public String getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(String players) {
+		this.players = players;
+	}
 }
